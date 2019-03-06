ac34ff5e22c1bd84eb4160fbb92aaa7729213e8e
Neeraj Joseph <joseph.neeraj@gmail.com>
2019-03-06 19:05:58 +0000

    Backup

--- commit.sh
+++ commit.sh
@@ -1,14 +1,19 @@
-while true
-do
+
 MESSAGE=$1
 if [ -z "$1"]
 then
 	MESSAGE="Backup"
 fi
 
+while true
+do
+echo "====================Commit start===================="
 echo "Committing with message $MESSAGE"
 git commit -am $MESSAGE
 git push origin master
-sleep 5
+
+echo "====================Commit end===================="
+
+sleep 120
 done
 
